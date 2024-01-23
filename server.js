c580c08663c121d2492423e53e4630bb72e434ab
noufanelachola <noufansadiqelachola@gmail.com>
2024-01-23 08:26:10 +0530

    db deploying

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt-nodejs");
 const knex = require("knex");
+require('dotenv').config();
+const { Client } = require('pg');
 
 const register = require("./controllers/register");
 const signIn = require("./controllers/signIn");
@@ -14,16 +16,60 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// const db = knex({
+//     client: "pg",
+//     connection: {
+//         host : '127.0.0.1',
+//         port : 5432,
+//         user : 'postgres',
+//         password : '9987',
+//         database : 'smartbrain'
+//     }
+// });
+
+// Load environment variables from .env file
+
+// dotenv.config();
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+// client.connect();
+
+const knex = require("knex");
+
 const db = knex({
-    client: "pg",
-    connection: {
-        host : '127.0.0.1',
-        port : 5432,
-        user : 'postgres',
-        password : '9987',
-        database : 'smartbrain'
-    }
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 });
+
+
+// const createLoginTableQuery = `
+//   CREATE TABLE IF NOT EXISTS login (
+//     id SERIAL PRIMARY KEY,
+//     hash VARCHAR(100) NOT NULL,
+//     email TEXT UNIQUE NOT NULL
+//   );
+// `;
+
+// // SQL query to create the 'users' table
+// const createUsersTableQuery = `
+//   CREATE TABLE IF NOT EXISTS users (
+//     id SERIAL PRIMARY KEY,
+//     name VARCHAR(100),
+//     email TEXT UNIQUE NOT NULL,
+//     entries BIGINT DEFAULT 0,
+//     joined TIMESTAMP NOT NULL
+//   );
+// `;
 
 // const saltRounds = 10;
 
