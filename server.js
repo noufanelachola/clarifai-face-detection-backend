790ba5080b9e4b62e33fcffaf8c22bb55de54aa0
noufanelachola <noufansadiqelachola@gmail.com>
2024-01-26 15:20:07 +0530

    uncommented listen

--- server.js
+++ server.js
@@ -56,6 +56,6 @@ app.get("/",(req,res) => {
 // app.post("/imageurl",(req,res) => {image.handleApiCall(req,res)});
 
 
-// app.listen(process.env.PORT || 3000,() => {
-//     console.log(`App is running on port ${process.env.PORT}`)
-// });
+app.listen(process.env.PORT || 3000,() => {
+    console.log(`App is running on port ${process.env.PORT}`)
+});
